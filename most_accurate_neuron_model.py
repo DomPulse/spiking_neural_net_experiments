73c1f47db760c63ea771f7b44beb012fe2d26fa3
Dominic Polsinelli <32404918+DomPulse@users.noreply.github.com>
2025-03-31 00:51:02 -0500

    Add files via upload
    atill not working

--- most_accurate_neuron_model.py
+++ most_accurate_neuron_model.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 #the only thing they vary is the conductance, I geuss I could vary more but let's copy them first
 #all voltages in mV, all times in ms
 #currents should be in nA
+#https://www.jneurosci.org/content/jneuro/18/7/2309.full.pdf
+#aparently same model^ ?
 
 def m_or_h_infin(V, top, bottom, Ca_adj = False, Ca = 1):
 	mult = 1
@@ -23,7 +25,7 @@ def double_exp_taus(V, numer, top_1, bottom_1, top_2, bottom_2, offset):
 	return offset + numer/denom
 
 def tau_h_Na(V):
-	first_mult = 1.34/(1+np.exp((V + 62.9)/(-10)))
+	first_mult = 0.67/(1+np.exp((V + 62.9)/(-10)))
 	second_mult = 1.5 + 1/(1 + np.exp((V + 34.9)/3.6))
 	return first_mult*second_mult
 
@@ -55,7 +57,7 @@ def m_h_stuff(all_params, V_membrane, Ca2, init_m_h = False):
 		all_params[3, 3] = m_or_h_infin(V_membrane, 27.2, -8.7)
 		all_params[4, 3] = m_or_h_infin(V_membrane, 28.3, -12.6, True, Ca2)
 		all_params[5, 3] = m_or_h_infin(V_membrane, 12.3, -11.8)
-		all_params[6, 3] = m_or_h_infin(V_membrane, 75, 5.5)
+		all_params[6, 3] = m_or_h_infin(V_membrane, 70, 6)
 
 		#and now all the h values
 		all_params[0, 4] = m_or_h_infin(V_membrane, 48.9, 5.18)
@@ -73,7 +75,7 @@ def m_h_stuff(all_params, V_membrane, Ca2, init_m_h = False):
 	all_params[3, 5] = m_or_h_infin(V_membrane, 27.2, -8.7)
 	all_params[4, 5] = m_or_h_infin(V_membrane, 28.3, -12.6, True, Ca2)
 	all_params[5, 5] = m_or_h_infin(V_membrane, 12.3, -11.8)
-	all_params[6, 5] = m_or_h_infin(V_membrane, 75, 5.5)
+	all_params[6, 5] = m_or_h_infin(V_membrane, 70, 6)
 
 	#and now all the h_infinity values
 	all_params[0, 6] = m_or_h_infin(V_membrane, 48.9, 5.18)
@@ -85,43 +87,48 @@ def m_h_stuff(all_params, V_membrane, Ca2, init_m_h = False):
 	all_params[6, 6] = 1
 
 	#setting all the tau_m values
-	all_params[0, 7] = most_taus(V_membrane, -2.52, 120, -25, 2.64)
-	all_params[1, 7] = most_taus(V_membrane, -42.6, 68.1, -20.5, 43.4)
-	all_params[2, 7] = double_exp_taus(V_membrane, 14, 27, 10, 70, -13, 2.8)
-	all_params[3, 7] = most_taus(V_membrane, -20.8, 32.9, -15.2, 23.2)
-	all_params[4, 7] = most_taus(V_membrane, -150.2, 46, -22.7, 180.6)
-	all_params[5, 7] = most_taus(V_membrane, -12.8, 28.3, -19.2, 14.4)
-	all_params[6, 7] = double_exp_taus(V_membrane, 2, 169.7, -11.6, -26.7, 14.3, 0)
+	all_params[0, 7] = most_taus(V_membrane, -1.26, 120, -25, 1.32)
+	all_params[1, 7] = most_taus(V_membrane, -21.3, 68.1, -20.5, 68.1)
+	all_params[2, 7] = double_exp_taus(V_membrane, 7, 27, 10, 70, -13, 14)
+	all_params[3, 7] = most_taus(V_membrane, -10.4, 32.9, -15.2, 11.6)
+	all_params[4, 7] = most_taus(V_membrane, -75.1, 46, -22.7, 90.3)
+	all_params[5, 7] = most_taus(V_membrane, -6.4, 28.3, -19.2, 7.2)
+	all_params[6, 7] = most_taus(V_membrane, 1499, 42.2, -8.73, 272)
 
 	#and now all the ta_h values
 	all_params[0, 8] = tau_h_Na(V_membrane)
-	all_params[1, 8] = most_taus(V_membrane, -179.6, 55, -16.9, 210)
-	all_params[2, 8] = double_exp_taus(V_membrane, 300, 55, 9, 65, -16, 120)
-	all_params[3, 8] = most_taus(V_membrane, -58.4, 38.9, -26.5, 77.2)
+	all_params[1, 8] = most_taus(V_membrane, -89.8, 55, -16.9, 105)
+	all_params[2, 8] = double_exp_taus(V_membrane, 150, 55, 9, 65, -16, 60)
+	all_params[3, 8] = most_taus(V_membrane, -29.2, 38.9, -26.5, 38.6)
 	all_params[4, 8] = 1
 	all_params[5, 8] = 1
 	all_params[6, 8] = 1
+
+	CaReverse = nernst(Ca2)
+	all_params[1:3, 1] = [CaReverse, CaReverse] #hey if anything is wrong check here I'm highly skeptical
 
 	return all_params
 
 A = 6.28E-4 #area in cm^2
 C = 6.28E-1 #capacitence in nF, something tells me they picked this to mostly cancel with area
-time_step = 0.5
-sim_length = 1000
+time_step = 0.05
+sim_length = 10000
 sim_steps = int(sim_length/time_step)
 
 #conductance in mS/cm^2 ?
-Na_g = 100
+Na_g = 400
 CaT_g = 0
-CaS_g = 10
+CaS_g = 8
 A_g = 50
 KCa_g = 20
-Kd_g = 100
+Kd_g = 50
 H_g = 0.04
-leak_g = 0.02
+leak_g = 0.05
 
 NaReverse = 50
-KReverse = -80
+KCaReverse = -80
+KdReverse = -80
+AReverse = -80
 HReverse = -20
 LeakReverse = -50
 
@@ -129,35 +136,37 @@ LeakReverse = -50
 V_membrane = -50
 Ca2 = 0.05
 CaReverse = nernst(Ca2)
-I_ext = 0.05
+I_ext = 0
 
 #I genuinely don't know if the time constants are meant to be constant, like yeah they should be but they are functions of V so like?
 #I think they do change bc of top of page 3 but like 
 #max conductance, reverse potential, p, m, h, m_infinity, h_infinity, tau_m, tau_h
 all_params = np.zeros((7, 9))
 all_params[:, 0] = [Na_g, CaT_g, CaS_g, A_g, KCa_g, Kd_g, H_g]
-all_params[:, 1] = [NaReverse, CaReverse, CaReverse, NaReverse, CaReverse, KReverse, HReverse] #hey if anything is wrong check here I'm highly skeptical
+all_params[:, 1] = [NaReverse, CaReverse, CaReverse, AReverse, KCaReverse, KdReverse, HReverse] #hey if anything is wrong check here I'm highly skeptical
 all_params[:, 2] = [3, 3, 3, 3, 4, 4, 1]
 all_params = m_h_stuff(all_params, V_membrane, Ca2, init_m_h = True)
 
 Vs = np.zeros(sim_steps)
 for s in range(sim_steps):
 	I_leak = (V_membrane - LeakReverse)*leak_g*A
-	current_sum = []
+	
 	all_params = m_h_stuff(all_params, V_membrane, Ca2)
 
 	for index, params in enumerate(all_params):
 		if index < 4:
-			all_params[index, 2] += time_step*(params[6] - params[4])/params[8]
-		all_params[index, 2] += time_step*(params[5] - params[3])/params[7]
+			all_params[index, 4] += time_step*(params[6] - params[4])/params[8]
+		all_params[index, 3] += time_step*(params[5] - params[3])/params[7]
 
+	current_sum = []
 	for params in all_params:
 		current_sum.append(current_contrib(A, V_membrane, params[1], params[0], params[3], params[2], params[4]))
 		
-	dV = time_step*(-1*np.sum(current_sum) - I_ext - I_leak)/C
+	dV = time_step*(-1*np.sum(current_sum) + I_ext - I_leak)/C
 	V_membrane += dV
 	Ca2 += step_Ca2(current_sum[1], current_sum[2], Ca2)
 	Vs[s] = V_membrane
+	#print(V_membrane, Ca2, dV, all_params[:, 3:5])
 
 plt.plot(Vs)
 plt.show()
